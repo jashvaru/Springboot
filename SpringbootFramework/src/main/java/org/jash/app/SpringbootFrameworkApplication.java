53f6bd500d38a7fe690c75b2a4b405b6fccc693b
jashvaru <jashvaru@gmail.com>
2024-10-03 15:35:20 +0530

    Example of Spring beans, Component and Component scan

--- SpringbootFramework/src/main/java/org/jash/app/SpringbootFrameworkApplication.java
+++ SpringbootFramework/src/main/java/org/jash/app/SpringbootFrameworkApplication.java
@@ -2,13 +2,18 @@ package org.jash.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.jash.component.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.jash.component"})
 public class SpringbootFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootFrameworkApplication.class, args);
-		System.out.println("Hello world");
+		ApplicationContext context = SpringApplication.run(SpringbootFrameworkApplication.class, args);
+		Student student = context.getBean(Student.class);
+		student.print();
 	}
 
 }
---
+++ SpringbootFramework/src/main/java/org/jash/component/Student.java
@@ -0,0 +1,10 @@
+package org.jash.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+    public void print() {
+        System.out.println("I am student");
+    }
+}
