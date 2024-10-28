b32973fc3e77b313d0b192ae6b4b4dbef0bf49ec
jashvaru <jashvaru@gmail.com>
2024-10-28 08:44:35 +0530

    Adding constructor

--- Spring1/src/main/java/com/jash/Alien.java
+++ Spring1/src/main/java/com/jash/Alien.java
@@ -2,6 +2,10 @@ package com.jash;
 
 public class Alien {
 
+    public Alien() {
+        System.out.println("Alien Obj Created");
+    }
+
     public void code() {
         System.out.println("coding....");
     }
