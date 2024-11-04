ca5d8de5a702472a3dc5559360c07efaae5edc7d
jashvaru <jashvaru@gmail.com>
2024-11-04 08:58:34 +0530

    Sending response to the http get req. sending response of type text/html

--- ServletEx/src/main/java/org/jash/HelloServlet.java
+++ ServletEx/src/main/java/org/jash/HelloServlet.java
@@ -4,11 +4,14 @@ package org.jash;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.SortedMap;
 
 public class HelloServlet extends HttpServlet {
 
-    public void service(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("In sevrice");
+        response.setContentType("text/html");
+        response.getWriter().println("<h2>Hello</h2>");
     }
 }
