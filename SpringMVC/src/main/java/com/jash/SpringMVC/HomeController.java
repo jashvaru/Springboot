666c042dfa98a7452dca2d79f69cc5a6320674ff
jashvaru <jashvaru@gmail.com>
2024-11-08 08:41:46 +0530

    Accepting request from browser and adding numbers

--- SpringMVC/src/main/java/com/jash/SpringMVC/HomeController.java
+++ SpringMVC/src/main/java/com/jash/SpringMVC/HomeController.java
@@ -1,5 +1,6 @@
 package com.jash.SpringMVC;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +11,15 @@ public class HomeController {
     public String home() {
         System.out.println("home page called");
         return "index.jsp";
+    }
+
+    @RequestMapping("/add")
+    public String add(HttpServletRequest request) {
+        System.out.println("add method called");
+        int num1 = Integer.parseInt(request.getParameter("num1"));
+        int num2 = Integer.parseInt(request.getParameter("num2"));
+        int result = num1 + num2;
+        System.out.println(result);
+        return "result.jsp";
     }
 }
