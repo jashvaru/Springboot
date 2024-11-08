e745ba3445fbcbfb2fdf513ee48031ac5d8c9379
jashvaru <jashvaru@gmail.com>
2024-11-08 08:51:00 +0530

    Sending response using session attribute and jsp

--- SpringMVC/src/main/java/com/jash/SpringMVC/HomeController.java
+++ SpringMVC/src/main/java/com/jash/SpringMVC/HomeController.java
@@ -1,6 +1,7 @@
 package com.jash.SpringMVC;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,12 +15,13 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(HttpServletRequest request) {
+    public String add(HttpServletRequest request, HttpSession session) {
         System.out.println("add method called");
         int num1 = Integer.parseInt(request.getParameter("num1"));
         int num2 = Integer.parseInt(request.getParameter("num2"));
         int result = num1 + num2;
         System.out.println(result);
+        session.setAttribute("result", result);
         return "result.jsp";
     }
 }
