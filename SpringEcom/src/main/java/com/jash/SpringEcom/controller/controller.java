7b3fb656eb66d1184c899d7231e830410b249231
jashvaru <jashvaru@gmail.com>
2024-11-23 23:27:25 +0530

    Get Product By Id

--- SpringEcom/src/main/java/com/jash/SpringEcom/controller/controller.java
+++ SpringEcom/src/main/java/com/jash/SpringEcom/controller/controller.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
@@ -32,5 +34,17 @@ public class controller {
     @GetMapping("/allproducts")
     public ResponseEntity<List<Product>> fetchAllProductsResponseEntity() {
         return new ResponseEntity<>(productService.fetchAllProducts(), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> fetchProductById(@PathVariable("id") int id) {
+        Product product = productService.fetchProductById(id).orElse(null);
+
+        if(Objects.nonNull(product)) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 }
--- SpringEcom/src/main/java/com/jash/SpringEcom/service/ProductService.java
+++ SpringEcom/src/main/java/com/jash/SpringEcom/service/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -15,5 +16,9 @@ public class ProductService {
 
     public List<Product> fetchAllProducts() {
         return productRepo.findAll();
+    }
+
+    public Optional<Product> fetchProductById(int id) {
+        return productRepo.findById(id);
     }
 }
