89d28183817a018081fa3a90f85767a43354909d
jashvaru <jashvaru@gmail.com>
2024-11-23 23:17:26 +0530

    Making use of ResponseEntity for control over http status code

--- SpringEcom/src/main/java/com/jash/SpringEcom/controller/controller.java
+++ SpringEcom/src/main/java/com/jash/SpringEcom/controller/controller.java
@@ -4,6 +4,8 @@ import com.jash.SpringEcom.model.Product;
 import com.jash.SpringEcom.repo.ProductRepo;
 import com.jash.SpringEcom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +27,10 @@ public class controller {
     @GetMapping("/products")
     public List<Product> fetchAllProducts() {
         return productService.fetchAllProducts();
+    }
+
+    @GetMapping("/allproducts")
+    public ResponseEntity<List<Product>> fetchAllProductsResponseEntity() {
+        return new ResponseEntity<>(productService.fetchAllProducts(), HttpStatus.ACCEPTED);
     }
 }
