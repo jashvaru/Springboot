b551a3d6da71e0dc9873f151502932a00cf9afb9
jashvaru <jashvaru@gmail.com>
2024-11-30 18:33:03 +0530

    Printing Session Id

--- SpringSecurity/src/main/java/com/jash/SpringSecurity/controller/Controller.java
+++ SpringSecurity/src/main/java/com/jash/SpringSecurity/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.jash.SpringSecurity.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello world";
+    public String hello(HttpServletRequest request) {
+        return "Hello world " + request.getSession().getId();
     }
+    /*
+    Session ID after logging in
+    Hello world 7321849B461593163E9AA66A013C311C
+    * */
 }
