523cf14c91d27bbfa0a69a2b9716444a423e0ed0
jashvaru <jashvaru@gmail.com>
2024-12-03 23:07:21 +0530

    Building HttpSecurity with multiple filter chain

--- SpringSecurity/src/main/java/com/jash/SpringSecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/jash/SpringSecurity/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.jash.SpringSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +15,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
     }
+
+
+
+
 }
