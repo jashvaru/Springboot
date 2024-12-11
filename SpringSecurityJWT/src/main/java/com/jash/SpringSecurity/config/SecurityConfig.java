41f7567f8c2fa92c2725500623b42d3f2bac1ac6
jashvaru <jashvaru@gmail.com>
2024-12-11 07:52:04 +0530

    Creating a login url for user

--- SpringSecurityJWT/src/main/java/com/jash/SpringSecurity/config/SecurityConfig.java
+++ SpringSecurityJWT/src/main/java/com/jash/SpringSecurity/config/SecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -42,13 +44,18 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("register")
+                        request.requestMatchers("register", "login")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
--- SpringSecurityJWT/src/main/java/com/jash/SpringSecurity/controller/UserController.java
+++ SpringSecurityJWT/src/main/java/com/jash/SpringSecurity/controller/UserController.java
@@ -3,6 +3,9 @@ package com.jash.SpringSecurity.controller;
 import com.jash.SpringSecurity.model.User;
 import com.jash.SpringSecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,10 +19,26 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("register")
     public User registerUser(@RequestBody User user) {
-        System.out.println("Insider User Controller");
+        System.out.println("Inside User Controller");
         return userService.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String loginUser(@RequestBody User user) {
+        System.out.println("Inside User login Controller");
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated()) {
+            return "Success";
+        } else {
+            return "Failed";
+        }
     }
 
     @GetMapping("user")
