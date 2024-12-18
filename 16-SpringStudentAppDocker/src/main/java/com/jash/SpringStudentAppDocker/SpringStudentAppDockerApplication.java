35859668fda10348fecb0b71143e5332137ed198
jashvaru <jashvaru@gmail.com>
2024-12-18 08:26:48 +0530

    Springboot student app docker

---
+++ 16-SpringStudentAppDocker/src/main/java/com/jash/SpringStudentAppDocker/SpringStudentAppDockerApplication.java
@@ -0,0 +1,13 @@
+package com.jash.SpringStudentAppDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentAppDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentAppDockerApplication.class, args);
+	}
+
+}
---
+++ 16-SpringStudentAppDocker/src/test/java/com/jash/SpringStudentAppDocker/SpringStudentAppDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jash.SpringStudentAppDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentAppDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
