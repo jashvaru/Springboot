d4b630244bcf0adc0b0f684a29dc22b6ef17e891
jashvaru <jashvaru@gmail.com>
2024-12-19 22:23:29 +0530

    Creating get request to fetch all students

--- 16-SpringStudentAppDocker/src/main/java/com/jash/SpringStudentAppDocker/controller/StudentController.java
+++ 16-SpringStudentAppDocker/src/main/java/com/jash/SpringStudentAppDocker/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.jash.SpringStudentAppDocker.model.Student;
 import com.jash.SpringStudentAppDocker.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
--- 16-SpringStudentAppDocker/src/main/java/com/jash/SpringStudentAppDocker/model/Student.java
+++ 16-SpringStudentAppDocker/src/main/java/com/jash/SpringStudentAppDocker/model/Student.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Entity
 @Table(name = "STUDENT_DOCKER")
-@Data
 public class Student {
 
     @Id
@@ -15,4 +14,37 @@ public class Student {
 
     private String name;
     private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
