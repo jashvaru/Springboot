93f238704733dbbe73d5cff9d2c8c00b02b2b0ad
jashvaru <jashvaru@gmail.com>
2024-12-28 17:19:28 +0530

    Quiz data from DB to response in controller

---
+++ 18-QuizApp/src/main/java/com/jash/QuizApp/model/QuizQuestion.java
@@ -0,0 +1,26 @@
+package com.jash.QuizApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "QuizQuestions")
+public class QuizQuestion {
+
+    @GeneratedValue
+    @Id
+    private int id;
+
+    private String category;
+    private String difficultyLevel;
+    private String question;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String ans;
+}
